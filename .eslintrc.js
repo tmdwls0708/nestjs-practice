0597d6aa6963a13dd6dbf6c65b4ee09cc74efb3f
sj.kim <sj.kim@jness.co.kr>
2025-01-22 08:52:22 +0900

    ts 오류 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      babelrc: false,
+      configFile: false,
+      presets: ['@babel/preset-env'],
+    },
+  },
+};
+
 // module.exports = {
 //   parser: '@typescript-eslint/parser',
 //   parserOptions: {
